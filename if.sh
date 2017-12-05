0c787257cfe52b0df32408e3b32360b2a3a35d45
oyh <625449191@qq.com>
2017-12-05 20:14:00 +0800

    add if.sh

---
+++ if.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then 
+    echo "usage:$0 filename"
+    exit 1
+fi
+
+file=$1
+
+if [ -d $file ]
+then
+    echo "$file is a directory"
+else
+    echo "$file is  not a directory"
+fi
